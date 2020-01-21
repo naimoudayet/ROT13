274c6b5dd9b5e75eb92f7953e567a299cfb8da8b
naiimoudayet <na.oudayet.im@gmail.com>
2020-01-21 09:53:39 +0100

    First Commit 21-01-2020

---
+++ app/src/main/java/io/droidtech/r0t13/ui/EncodeFrag.java
@@ -0,0 +1,30 @@
+package io.droidtech.r0t13.ui;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import io.droidtech.r0t13.R;
+
+public class EncodeFrag extends Fragment {
+
+    public EncodeFrag() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.encode_frag, container, false);
+    }
+
+}
